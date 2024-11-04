e5b58cd004889271aaaee1cda68e7a3dda1a1ed1
Nadia Alya Paramitha <nalya2911@gmail.com>
2024-11-04 13:38:34 +0700

    MENAMBAHKAN FOLDER

---
+++ lib/onboarding/onboarding_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
+
+class OnboardingView extends StatefulWidget {
+  const OnboardingView({super.key});
+
+  @override
+  State<OnboardingView> createState() => _OnboardingViewState();
+}
+
+class _OnboardingViewState extends State<OnboardingView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Onboarding"),
+    );
+  }
+}
